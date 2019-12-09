db57067807c85b1e38a21cf64409e26fd948bd5c
Bryant Vail <Bryanttv@outlook.com>
2019-12-09 09:08:15 -0500

    complete

---
+++ hamburger-menu/Animated-Hamburger-Menu-icon-with-Pure-CSS/readme/resources.js
@@ -0,0 +1,16 @@
+resources = [
+  {
+    name: "instructions for procject",
+    url: "https://www.cssScript.com/css-hamburger-toggle-menu/"
+  },
+  {
+    name: "ResearchItems",
+    items: [
+      {
+        name: "understanding IA",
+        url: "https://slickplan.com/blog/understanding-digital-design-terms-ia-ui-and-ux"
+    
+      }
+    ]
+  }  
+];
